3ba48204f01a3ea39af25d788aa03c04c26e876a
szymonaszek01 <235879@edu.p.lodz.pl>
2024-07-02 16:39:10 +0200

    Adding components and pages package

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div
+      className={`w-full h-full min-h-[100vh] flex items-center justify-center text-center`}>
+      <p className={`text-yellow-gradient font-semibold text-xl`}>Welcome in Ann's portfolio page!</p>
+    </div>
+  );
+};
+
+export default HomePage;
